90b461fc29c60e8cbe0750cb52acc4a6d28c4fcd
Daniel A. R. Werner <daniel.werner@contentful.com>
2018-09-20 16:49:01 +0200

    Implement migrations

--- index.js
+++ index.js
@@ -4,17 +4,78 @@
 module.exports = function (migration, {makeRequest}) {
   migration.transformEntries({
     contentType: 'lesson',
-    from: ['modules'],
+    from: ['title', 'modules'],
     to: ['stField'],
     transformEntryForLocale: async function (fromFields, currentLocale) {
-      const ids = fromFields.modules['en-US'].map(e => e.sys.id).join(',')
-      const entries = await makeRequest({
+      const ids = fromFields.modules['en-US'].map(e => e.sys.id)
+      const unsortedEntries = await makeRequest({
         method: 'GET',
-        url: `/entries?sys.id[in]=${ids}`
-      }) 
+        url: `/entries?sys.id[in]=${ids.join(',')}`
+      })
 
-      const lessonCopy = entries.items.filter(e => e.sys.contentType.sys.id === 'lessonCopy')
-      debugger;
+      const lessonBlocks = ids.map((id) => unsortedEntries.items.find(entry => entry.sys.id === id))
+      const lessonStructuredTextBlocks = lessonBlocks.map(transformLessonBlock)
+
+      return {
+        stField: {
+          nodeType: 'document',
+          nodeClass: 'document',
+          content: lessonStructuredTextBlocks
+        }
+      }
+
+      function transformLessonBlock (lessonBlock) {
+        switch (lessonBlock.sys.contentType.sys.id) {
+          case 'lessonCopy':
+            return transformLessonCopy(lessonBlock)
+          case 'lessonImage':
+            return transformLessonImage(lessonBlock)
+          case 'lessonCodeSnippets':
+            return transformLessonCodeSnippets(lessonBlock)
+        }
+      }
+
+      function transformLessonCopy (lessonBlock) {
+        // TODO: copy is a Markdown field! Use Markdown->ST transformer on it!
+        const copy = lessonBlock.fields.copy[currentLocale]
+        return {
+          nodeClass: 'block',
+          nodeType: 'paragraph',
+          content: [
+            {
+              nodeClass: 'text',
+              nodeType: 'text',
+              value: copy,
+              marks: []
+            },
+          ]
+        }
+      }
+
+      function transformLessonImage (lessonBlock) {
+        return createEntryBlockEmbed(lessonBlock)
+      }
+
+      function transformLessonCodeSnippets (lessonBlock) {
+        return createEntryBlockEmbed(lessonBlock)
+      }
     }
-  });
-};
+  })
+}
+
+function createEntryBlockEmbed (entry) {
+  const target = {
+    sys: {
+      type: 'Link',
+      linkType: 'Entry',
+      id: entry.sys.id
+    }
+  }
+  return {
+    nodeClass: 'block',
+    nodeType: 'embedded-entry-block',
+    data: {
+      target
+    }
+  }
+}
